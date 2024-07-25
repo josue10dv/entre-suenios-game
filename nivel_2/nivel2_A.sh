408377c678c77e598d67f6f5cd13153620ed1f24
josue10dv <topicig@gmail.com>
2024-07-25 18:37:39 -0500

    correccion ruta nivel 2 opcion 3

--- nivel_2/nivel2_A.sh
+++ nivel_2/nivel2_A.sh
@@ -2,7 +2,7 @@
 
 cat ./nivel_2/revelacion.txt
 
-rutas_niveles=("./nivel_2/final_1.sh" "./nivel_2/op_alicia_ninia.sh" "./nivel_2/final_complementario.sh")
+rutas_niveles=("./nivel_2/final_1.sh" "./nivel_2/op_alicia_ninia.sh" "./nivel_2/final_2.sh")
 opciones_disponibles=("1. Caminar hacia la Naciones Unidas" "2. Consolar a Alicia niña" "3. Hablar con Alicia adulta")
 
 bash ./validar_opcion.sh "${rutas_niveles[@]}" "${opciones_disponibles[@]}"
