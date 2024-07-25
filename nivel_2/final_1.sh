84d0089266b1236f4dddeb53b75a21ee885b40d8
josue10dv <topicig@gmail.com>
2024-07-25 18:36:46 -0500

    actualizacion de la historia

---
+++ nivel_2/final_1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Todas estas emociones y toda esta incertidumbre empiezan a abrumarte. Toda esta aura misteriosa, con cosas que no tienen sentido, parece 
+que no va a desaparecer a menos que tomes una decisión. Quieres estar tranquilo y no tener más pesadillas, así que determinas que tu mejor 
+opción es caminar directamente hacia donde todo comenzó y superar lo vivido. Prefieres dejar de lado la coincidencia de que el recuerdo fuera 
+real y volver al escepticismo al que estás acostumbrado.
+
+Despiertas súbitamente al poner un pie en el bulevar. La música hace su infaltable presencia, lo que te alivia; la pesadilla ha terminado."
+
+bash ./nivel_2/final_general.sh
---
+++ nivel_2/final_general.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Durante el día, recibiste varias visitas de pacientes. Aunque los atendiste con cordialidad y profesionalismo, en realidad esperabas ver 
+a Alicia. Cuando finalmente llegó, tuvieron una interacción, aunque distante. No encontraste la manera de ayudarla, y ella se marchó tan 
+tranquila como había llegado. Diste lo mejor de ti, pero aún les quedan varias sesiones, así que mantienes la esperanza.
+
+Alicia se fue en su vehículo. En el camino a casa, sufrió un accidente y falleció. Te destroza descubrir ese desenlace. Al parecer, no era 
+necesario intentar quitarse la vida; el destino ya había previsto ese trágico final...
+
+FIN"
