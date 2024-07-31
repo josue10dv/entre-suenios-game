efc6875c83991563b91a2aecc8dc372627b50951
josue10dv <topicig@gmail.com>
2024-07-30 19:14:49 -0500

    final canon

---
+++ nivel_2/final_2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Te acercas a Alicia adulta, ella está impasible fumando su cigarrillo. De alguna manera, hablan sobre el sinsentido de la vida. Alicia hace un 
+recuento de todos los recuerdos que has podido ver entre sueños durante los últimos días. Recuerda el abuso de su padre hacia ella y su madre, 
+cómo su madre enloqueció cuando su padre murió en un accidente de tránsito por estar conduciendo en estado etílico. Ella expreso que nunca hubiera
+creído que el gentil y amoroso espíritu de su madre se viera trastornado por tantos años de abusos. Comprendes su gran depresión y estás seguro de que puedes 
+ayudarla a salir de ese oscuro agujero. Sabes lo que debes hacer, el camino que tomar, pero hay algo en su expresión. A pesar de que Alicia se 
+abriera emocionalmente, su rostro lucía inexpresivo, no mostraba ni el más mínimo ápice de esperanza. Por algún motivo, no puedes pronunciar ni 
+una palabra, por lo que solo puedes escuchar. De pronto, te clava la mirada y ves cómo sus labios pronuncian un fúnebre \"Gracias por al menos 
+intentarlo\".... todo se vuelve oscuro. Los Peppers harán el anuncio de que la hora de despertar ha lledado."
+
+bash ./nivel_2/final_general.sh
---
+++ nivel_2/op_alicia_ninia.sh
@@ -0,0 +1,32 @@
+echo "Ves cómo Alicia llora desconsoladamente pero sin emitir un solo sonido. Es como si un aire extraña silenciara cualquier sonido en un 
+pequeño diámetro a su alrededor. Cuando estás a pocos pasos de ella, empiezas a escuchar sus lamentos: \"¿Por qué? ¿Por qué estoy tan sola? 
+¿Por qué no puedo ser feliz?\". Te estremeces; comprendes perfectamente cómo las emociones negativas e intensas pueden causar traumas tan 
+intrincados en la mente que pueden resultar totalmente inconscientes. Lamentas que Alicia haya tenido que vivir todo eso a tan temprana edad. 
+Cada momento sientes una conexión con ella, una extraña necesidad casi paternal de tranquilizar su tormenta. No la abrumas con palabras; te 
+agachas frente a ella y la observas con empatía, esperando que esté lista para expresar sus emociones contigo. Alicia levanta la mirada y te 
+observa con entendimiento, sabe que quieres ayudarla. Estiras tu brazo para tocar su hombro y, cuando lo haces, puedes visualizar, como una 
+especie de película, un recuerdo de su infancia.
+
+Es una tarde lluviosa y Alicia está feliz de que su padre no ha vuelto y está únicamente con su madre. Ella juega tranquilamente en la sala de 
+estar mientras observa cómo su madre prepara una deliciosa comida. De pronto, unas luces de auto iluminan la habitación: su padre está de 
+vuelta. La puerta principal se abre violentamente. Su padre entra tambaleándose, mojado y con un olor insoportable a vodka. Va directo hacia la 
+cocina donde discute con su madre. La situación escala y huyes a tu habitación. Escuchas cómo su padre se va nuevamente. Todo se llena de una 
+neblina extraña y espesa; sientes una sensación de caída y paras en seco en otro lugar. Ahora estás en el auto con Alicia y su madre, que va 
+hacia el hospital. Cuando llegan, reciben la noticia de que la situación del padre es crítica; está perdiendo mucha sangre y creen que no hay 
+salvación aunque luchan por estabilizarlo. Todos sus esfuerzos son en vano. Los días en el recuerdo pasan en un parpadeo y ves cómo la condición 
+mental de su madre va decayendo. La relación madre e hija se destruye por la violencia y, como adolescente, Alicia huye.
+
+La ves nuevamente como una niña; has vuelto al punto de partida, cuando tocaste su hombro. Empiezas a hablar; las palabras fluyen de forma 
+incontenible. Ves en sus ojos todas las emociones que produce escuchar tu monólogo, su expresión cambia de forma esperanzadora. Ella sonríe y te
+dice: \"despierta\".
+
+Esa misma tarde, recibes a Alicia en el consultorio. De alguna manera, vuelves a repetir las mismas palabras que en tu sueño, obteniendo el 
+mismo resultado, aunque esta vez únicamente puedes ver a una Alicia con los ojos llorosos sonriendo y preguntándote si quieres acompañarla a 
+reencontrarse con su madre. Aceptas y descubres que su nombre es Jenna; vive en la misma casa que viste en tus sueños, aunque con un ambiente 
+menos lúgubre. Al igual que Alicia, Jenna ha cruzado por un periodo de sanación, lo que se refleja en su conversación fructífera, empática y con
+un desenlace positivo. Se marchan con nuevos aires. Alicia no dice una sola palabra, pero puedes ver un gran sentido de paz en su aura.
+
+Pasan siete años y recuerdas cómo todo empezó. Alicia se ha convertido en una buena amiga y has visto cómo su hija, Johana, ha crecido en un 
+ambiente mucho más amoroso y apacible.
+
+FIN"
